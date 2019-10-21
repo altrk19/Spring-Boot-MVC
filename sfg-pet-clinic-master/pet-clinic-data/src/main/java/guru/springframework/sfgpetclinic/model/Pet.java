895b4c7dd12c0ee0d49c038fbb1812af492b60f4
aturk <aturk@netas.com.tr>
2019-10-21 20:07:13 +0300

    added base entity

--- sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
