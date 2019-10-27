614eee02eaaa7e8d4cdddbde35019d0424bb878a
aturk <aturk@netas.com.tr>
2019-10-27 20:39:41 +0300

    added XML configuration

--- jokeapp/src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
+++ jokeapp/src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -2,8 +2,11 @@ package guru.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication public class JokeappApplication {
+@SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
+public class JokeappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JokeappApplication.class, args);
