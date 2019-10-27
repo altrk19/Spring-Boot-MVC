44c3ddca04da4b52ec231e375d1acf9ccd7b026e
aturk <aturk@netas.com.tr>
2019-10-27 19:21:21 +0300

    Java Based @Configuration

--- jokeapp/src/main/java/guru/springframework/joke/jokeapp/controller/JokeController.java
+++ jokeapp/src/main/java/guru/springframework/joke/jokeapp/controller/JokeController.java
@@ -15,9 +15,9 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""})
-    public String showJoke(Model model){
-        model.addAttribute("joke",jokeService.getJoke());
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
 }
