d4baaad39452b86ca66e5dc81efe54d1741943fa
aturk <aturk@netas.com.tr>
2019-10-29 14:51:19 +0300

    added Spring external properties - yaml - multiproperties

--- di-demo2/src/main/java/guru/springframework/didemo2/didemo2/DiDemo2Application.java
+++ di-demo2/src/main/java/guru/springframework/didemo2/didemo2/DiDemo2Application.java
@@ -3,6 +3,8 @@ package guru.springframework.didemo2.didemo2;
 import guru.springframework.didemo2.didemo2.controllers.ConstructorInjectedController;
 import guru.springframework.didemo2.didemo2.controllers.MyController;
 import guru.springframework.didemo2.didemo2.controllers.SetterInjectedController;
+import guru.springframework.didemo2.didemo2.datasourcebean.FakeDataSource;
+import guru.springframework.didemo2.didemo2.datasourcebean.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +21,16 @@ public class DiDemo2Application {
 
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
         System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+        System.out.println("Data Source");
+        FakeDataSource fakeDataSource = (FakeDataSource)ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getPassword());
+
+        System.out.println("JMS Broker");
+        FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+        System.out.println(fakeJmsBroker.getJmsUser());
+        System.out.println(fakeJmsBroker.getJmsPassword());
     }
 
 }
