03213f8584b663b57fe12675e93055da5ec63da4
aturk <aturk@netas.com.tr>
2019-11-09 21:55:30 +0300

    added relations and entities

---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+}
---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne            //didnt use cascade. Because we dont want delete recipe if deleted notes
+    private Recipe recipe;
+
+    @Lob       //for large objects : string length more than 255   //clob:character lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
