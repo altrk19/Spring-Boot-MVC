1a95e2df47858067fc7e0c4d90d3fa9a13149f10
aturk <aturk@netas.com.tr>
2019-11-09 22:07:29 +0300

    added UnitOfMeasure

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -16,4 +16,46 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne(fetch = FetchType.EAGER)     //EAGER is default behavior
+    private UnitOfMeasure uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 }
---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String description;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
