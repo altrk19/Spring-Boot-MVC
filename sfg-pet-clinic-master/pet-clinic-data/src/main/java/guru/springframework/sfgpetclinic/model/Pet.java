4dbb77d1ad3dd095aab4d51a0d8c01157754d02d
aturk <aturk@netas.com.tr>
2019-11-11 21:25:52 +0300

    added JPA entities

--- sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,13 +1,29 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
-
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")           //bi-directional mappingde, cascadeType ve mapped by kullanılıyor. mappedby ile
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -39,5 +55,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
