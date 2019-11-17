5eff6ff8647b4fd42d6db2951f4625fe652d329c
aturk <aturk@netas.com.tr>
2019-11-17 18:55:49 +0300

    added extendWith annotation to recipe app

---
+++ sfg-pet-clinic-master/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SfgPetClinicApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
--- spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/repository/UnitOfMeasureRepositoryIT.java
+++ spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/repository/UnitOfMeasureRepositoryIT.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
     @Autowired
