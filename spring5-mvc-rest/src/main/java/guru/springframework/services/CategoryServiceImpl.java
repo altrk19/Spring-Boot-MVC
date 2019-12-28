5aeb10073f7d4b9111cd1bcd94a63253e2c1f99b
aturk <aturk@netas.com.tr>
2019-12-28 23:07:17 +0300

    edit

--- spring5-mvc-rest/src/main/java/guru/springframework/services/CategoryServiceImpl.java
+++ spring5-mvc-rest/src/main/java/guru/springframework/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package guru.springframework.services;
 import guru.springframework.api.v1.mapper.CategoryMapper;
 import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryMapper categoryMapper;
     private final CategoryRepository categoryRepository;
