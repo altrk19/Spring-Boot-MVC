a7ef4ffd51b1de842c5f388915fc2a1d93210d73
aturk <aturk@netas.com.tr>
2019-12-29 12:39:45 +0300

    spring5-mvc-rest added update customer API

--- spring5-mvc-rest/src/main/java/guru/springframework/controller/CustomerController.java
+++ spring5-mvc-rest/src/main/java/guru/springframework/controller/CustomerController.java
@@ -21,20 +21,26 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<CustomerListDTO> getListofCustomers() {
 
-        return new ResponseEntity<CustomerListDTO>(new CustomerListDTO(customerService.getAllCustomers()),
+        return new ResponseEntity<>(new CustomerListDTO(customerService.getAllCustomers()),
                 HttpStatus.OK);
 
     }
 
     @GetMapping({"/{id}"})
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
-        return new ResponseEntity<CustomerDTO>(customerService.getCustomerById(id), HttpStatus.OK);
+        return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
     }
 
 
     @PostMapping
     public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
-        return new ResponseEntity<CustomerDTO>(customerService.createNewCustomer(customerDTO),
+        return new ResponseEntity<>(customerService.createNewCustomer(customerDTO),
                 HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{id}"})
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
+        return new ResponseEntity<>(customerService.saveCustomerByDTO(id, customerDTO),
+                HttpStatus.OK);
     }
 }
--- spring5-mvc-rest/src/main/java/guru/springframework/services/CustomerServiceImpl.java
+++ spring5-mvc-rest/src/main/java/guru/springframework/services/CustomerServiceImpl.java
@@ -48,7 +48,22 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerDTO createNewCustomer(CustomerDTO customerDTO) {
+        return saveAndReturnDTO(customerMapper.customerDtoToCustomer(customerDTO));
+    }
+
+    @Override
+    public CustomerDTO saveCustomerByDTO(Long id, CustomerDTO customerDTO) {
         Customer customer = customerMapper.customerDtoToCustomer(customerDTO);
+        customer.setId(id);
+        return saveAndReturnDTO(customer);
+    }
+
+    /**
+     *
+     * @param customer customer
+     * @return CustomerDTO
+     */
+    private CustomerDTO saveAndReturnDTO(Customer customer) {
         Customer savedCustomer = customerRepository.save(customer);
         CustomerDTO returnDto = customerMapper.customerToCustomerDTO(savedCustomer);
         returnDto.setCustomerUrl("/api/v1/customer/" + savedCustomer.getId());
