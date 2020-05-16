764eed8a79efa236cd856a42073dbaeb18270dc7
aturk <aturk@netas.com.tr>
2020-05-16 16:52:54 +0300

    moved reactive examples

--- spring5-reactive-mongo-recipe-app-reactive-repos/README.md
+++
@@ -1,3 +0,0 @@
-# spring5-reactive-mongo-recipe-app
-Reactive Recipe Application Using MongoDB
-
--- spring5-reactive-mongo-recipe-app-reactive-repos/src/main/java/guru/springframework/repositories/reactive/RecipeReactiveRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories.reactive;
-
-import guru.springframework.domain.Recipe;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface RecipeReactiveRepository extends ReactiveMongoRepository<Recipe, String>{
-}
